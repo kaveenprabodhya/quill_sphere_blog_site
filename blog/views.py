ce496f43224717e016d3e8c4d90f685af7a84213
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2024-05-05 00:11:22 +0530

    config blog data and models

--- blog/views.py
+++ blog/views.py
@@ -1,60 +1,5 @@
-from django.shortcuts import render
-from datetime import date
-
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Quill Sphere",
-        "date": date(2024, 4, 30),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the view you get when hiking in the mountains, And I wasn't even prepared for what happend whilst I was enjoying the view!",
-        "content": """
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus minus nam unde nostrum at, voluptatum quaerat ipsam et natus saepe modi quis neque iure voluptate, eius labore, dolor ab rerum.
-        
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus minus nam unde nostrum at, voluptatum quaerat ipsam et natus saepe modi quis neque iure voluptate, eius labore, dolor ab rerum.
-        
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus minus nam unde nostrum at, voluptatum quaerat ipsam et natus saepe modi quis neque iure voluptate, eius labore, dolor ab rerum.
-        
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus minus nam unde nostrum at, voluptatum quaerat ipsam et natus saepe modi quis neque iure voluptate, eius labore, dolor ab rerum.
-        
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus minus nam unde nostrum at, voluptatum quaerat ipsam et natus saepe modi quis neque iure voluptate, eius labore, dolor ab rerum.
-        
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus minus nam unde nostrum at, voluptatum quaerat ipsam et natus saepe modi quis neque iure voluptate, eius labore, dolor ab rerum.
-        """,
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Quill Sphere",
-        "date": date(2024, 1, 3),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis aperiam est praesentium, quos iste consequuntur omnis exercitationem quam velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis aperiam est praesentium, quos iste consequuntur omnis exercitationem quam velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis aperiam est praesentium, quos iste consequuntur omnis exercitationem quam velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """,
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Quill Sphere",
-        "date": date(2023, 6, 18),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis aperiam est praesentium, quos iste consequuntur omnis exercitationem velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis aperiam est praesentium, quos iste consequuntur omnis exercitationem quam velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis aperiam est praesentium, quos iste consequuntur omnis exercitationem quam velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """,
-    },
-]
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 def get_date(post):
     return post["date"]
@@ -64,15 +9,18 @@ def get_date(post):
 
 
 def index(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {"posts": latest_posts})
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {"all_posts": all_posts})
 
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
-    return render(request, "blog/post-detail.html", {"post": identified_post})
+    identified_post = get_object_or_404(Post, slug=slug)
+    return render(request, "blog/post-detail.html", {
+        "post": identified_post, 
+        "post_tags": identified_post.tags.all()
+    })
