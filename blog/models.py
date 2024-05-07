a623a2784df8cc563b82b65adf59945376695e75
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2024-05-08 00:17:14 +0530

    config forms, files, sessions

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,13 @@ from django.core.validators import MinLengthValidator
 
 # Create your models here.
 
+
 class Tag(models.Model):
     caption = models.CharField(max_length=30)
 
     def __str__(self):
         return self.caption
-    
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
@@ -20,14 +21,27 @@ class Author(models.Model):
 
     def __str__(self):
         return self.full_name()
-    
+
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL,null=True, related_name="posts")
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, null=True, related_name="posts"
+    )
     tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return f"{self.title}"
+    
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
